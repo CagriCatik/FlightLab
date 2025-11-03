3b06079bf2bdaf91d57095c336f0c9fff671f687
CagriCatik <CagriCatik@users.noreply.github.com>
2025-11-03 12:41:45 +0000

    deploy: be174620065dbb7a1de0b09fd76501c0256883bc

---
+++ javascripts/analytics.js
@@ -0,0 +1,8 @@
+// Basic Google Analytics initialization
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+
+document.addEventListener("DOMContentLoaded", function() {
+  gtag("js", new Date());
+  gtag("config", "G-XXXXXXXXXX");
+});
